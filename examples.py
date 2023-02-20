c8387d216d0c3b22fb5de4cd2d3c08e160933ae1
Narick Amezcua <124534743+narickamez@users.noreply.github.com>
2023-02-19 17:38:43 -0800

    Create examples.py
    These are some examples you can use to test your code.

---
+++ examples.py
@@ -0,0 +1,11 @@
+# Example 1: Test the solution function with a binary tree of height 3 and a list of target node labels [7, 3, 5, 1].
+# The expected output is [-1, 7, 6, 3].
+print(solution(3, [7, 3, 5, 1]))
+
+# Example 2: Test the solution function with a binary tree of height 5 and a list of target node labels [19, 14, 28].
+# The expected output is [21, 15, 29].
+print(solution(5, [19, 14, 28]))
+
+# Example 3: Test the solution function with a binary tree of height 3 and a list of target node labels [1, 4, 7].
+# The expected output is [3, 6, -1].
+print(solution(3, [1, 4, 7]))
